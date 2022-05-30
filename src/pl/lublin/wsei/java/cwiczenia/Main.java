b3c08ac7ff9de0a7ff1b593adf99bcd5c689e825
Aalleeexx <dragher402@gmail.com>
2022-05-30 18:24:21 +0200

    6h

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,12 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+public class Main {
+    public static void main(String[] args) {
+
+       int a = 3;
+       double b = 4.21;
+       String s = "jaiś tekst";
+
+       System.out.printf("a =%d, b =%.2f, s = %20s %n",a,b,s);
+    }
+}
